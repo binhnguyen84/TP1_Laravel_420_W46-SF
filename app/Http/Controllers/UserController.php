8ea15f81b5157bc63fd8d577f89cb1e887f9aa06
Binh Nguyen <tienbinhnguyen84@gmail.com>
2023-04-03 23:47:11 -0400

    add function modify user info

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use App\Models\Role;
 use App\Http\Resources\UserResource;
 
 
@@ -61,5 +62,62 @@ class UserController extends Controller
             abort(401,'Interdit');
         } 
         return Auth::user();  
+    }
+
+    public function edit(Request $request)
+    {
+        if (!Auth::check()) {
+            abort(401,'Vous devez vous déconnecter avant de modifier votre compte utilisateur');
+        }
+        
+        //get the authenticated userid
+        $id = Auth::id();
+
+        //get the user to be updated
+        $userToUpDate = User::findOrFail($id);
+
+        //verify input data in request
+        try {
+            
+            $validatedData = $request->validate([
+                'last_name' => 'required',
+                'first_name' => 'required',
+                'password' => 'required|min:6',
+                'role_id' => 'required|exists:roles,id',
+                //'new_password'=> 'nullable',
+                //'email' => 'required',
+            ]);
+            
+        } catch (\Throwable $th) {
+            abort(400,'requête invalide');
+        }
+        
+        //get input data
+        $last_name = $validatedData['last_name'];
+        $first_name = $validatedData['first_name'];
+        $password = $validatedData['password'];
+        $role_id = $validatedData['role_id'];
+
+          // verify credentials data using Auth::attempt
+        if (!Auth::attempt(['email' => $userToUpDate->email, 'password' => $password])) {
+            abort(403,'Non Authorisé');
+        }
+
+        $userToUpdate->fill([
+            'last_name' => $last_name,
+            'first_name'=> $first_name,
+            'role_id'=> $role_id
+        ]);
+        //Revoke user tokens
+        $userToUpDate ->tokens()->delete();
+        
+        //generate new token
+        $token = $userToUpDate ->creataToken('authToken')->plainTextToken;
+        
+        return response()->Json([
+            'message' => 'OK',
+            'utilisateur' => $userToUpdate,
+            'token' => $token
+        ]);
     }
 }
