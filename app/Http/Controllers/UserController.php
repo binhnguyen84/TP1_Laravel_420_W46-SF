1ee01fd62f3aefe290dd42034139b1b54e48419d
Binh Nguyen <tienbinhnguyen84@gmail.com>
2023-04-04 22:44:03 -0400

    modify edit fucntion to change password

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -55,57 +55,55 @@ class UserController extends Controller
             abort(401,'Vous devez vous déconnecter avant de modifier votre compte utilisateur');
         }
         
-        //get the authenticated userid
-        $id = Auth::id();
-
-        //get the user to be updated
-        $userToUpDate = User::findOrFail($id);
+        $userToUpDate = Auth::user();
 
         //verify input data in request
-        try {
-            
-            $validatedData = $request->validate([
-                'last_name' => 'required',
-                'first_name' => 'required',
-                'password' => 'required|min:6',
-                'role_id' => 'required|exists:roles,id',
-            ]);
-            
-        } catch (\Throwable $th) {
-            abort(400,'requête invalide');
-        }
+        $request->validate([
+            'last_name' => 'required',
+            'first_name' => 'required',
+            'role_id' => 'required|exists:roles,id',
+        ]);     
         
         //get input data
-        $last_name = $validatedData['last_name'];
-        $first_name = $validatedData['first_name'];
-        $password = $validatedData['password'];
-        $role_id = $validatedData['role_id'];
-
-          // verify credentials data using Auth::attempt
-        if (!Auth::attempt(['email' => $userToUpDate->email, 'password' => $password])) {
-            abort(403,'Non Authorisé');
-        }
-
-        $userToUpdate->fill([
+        $last_name = $request->input('last_name');
+        $first_name = $request->input('first_name');
+        $role_id = $request->input('role_id');
+        
+        $userToUpDate->fill([
             'last_name' => $last_name,
             'first_name'=> $first_name,
             'role_id'=> $role_id
         ]);
+
+        // if request a change of password
+        if ($request->has('old_password') && $request->has('new_password')) {
+            
+            $newPassword = $request->input('new_password');
+            $oldPassword = $request->input('old_password');
+            $email = $userToUpDate->email;
+            if(!Auth::attempt(['email' =>$email,'password' => $oldPassword])){
+                abort(403,'Non authorisé');
+            }
+            $userToUpDate->password = bcrypt($newPassword);
+            $userToUpDate -> save();
+        }
+
+                
         //Revoke user tokens
         $userToUpDate ->tokens()->delete();
         
-        if ($user ->role->name == 'admin') {
-            $token = $user->createToken('admin',['films:post','films:delete'])->plainTextToken;
+        if ($userToUpDate ->role->name == 'admin') {
+            $token = $userToUpDate->createToken('admin',['films:post','films:delete'])->plainTextToken;
             
         }else{
 
-            $token = $user->createToken('member',[''])->plainTextToken;
+            $token = $userToUpDate->createToken('member',[''])->plainTextToken;
         }
 
         // return result
         return response()->Json([
             'message' => 'OK',
-            'utilisateur' => $user,
+            'utilisateur' => $userToUpDate,
             'token' => $token
         ],201);
     }
